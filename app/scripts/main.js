9fe4743b23cb5948eaa1786e8d542c1380d555b5
Cheng <jack.cheng@relaxpsy.cn>
2016-12-02 21:35:42 +0800

    add objloader model

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,11 +12,13 @@ class Ctx {
     $('#canvas')[0].width = this.width
     $('#canvas')[0].height = this.height
     renderer = new THREE.WebGLRenderer({
-      canvas: $('#canvas')[0]
+      canvas: $('#canvas')[0],
+      antialias:true,
+      maxLights:1,
     })
     scene = new THREE.Scene()
-    camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1000)
-    camera.position.set(60, 60, 100)
+    camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 4000)
+    camera.position.set(600, 600, 1000)
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     scene.add(camera)
     this.initGrid()
@@ -36,18 +38,18 @@ class Ctx {
 let ctx = new Ctx()
 ctx.init()
 //model
-geometry = new THREE.CubeGeometry(20, 20, 20, 20, 20)
-material = new THREE.MeshLambertMaterial({
-  color: 0x357FBE,
-  emissive: 0x9AD3EC,
-  wireframe: true
-})
-model = new THREE.Mesh(geometry, material)
-scene.add(model)
+// geometry = new THREE.CubeGeometry(20, 20, 20, 20, 20)
+// material = new THREE.MeshLambertMaterial({
+//   color: 0x357FBE,
+//   emissive: 0x9AD3EC,
+//   wireframe: true
+// })
+// model = new THREE.Mesh(geometry, material)
+// scene.add(model)
 
 //light
-let light = new THREE.DirectionalLight(0xffffff, .5)
-light.position.set(10, 20, 50 )
+let light = new THREE.DirectionalLight( 0xffffff, 0.5 );
+light.position.set( 0, 1, 0 );
 scene.add(light)
 
 //line
@@ -61,10 +63,28 @@ line = new THREE.Line(lineGeometry, new THREE.LineBasicMaterial({
   color: 0xFC0302
 }))
 scene.add(line);
-(function animate() {
+
+let mtlModel = new THREE.MTLLoader()
+mtlModel.setPath('../images/OBJ/tex/');
+mtlModel.load('../alduin.mtl', (mtl)=>{
+  mtl.preload()
+  let objModel =  new THREE.OBJLoader()
+  objModel.setMaterials(mtl);
+  objModel.load('../images/OBJ/alduin.obj', (obj)=>{
+    console.log(obj);
+
+    obj.name='dragon'
+    scene.add(obj)
+    renderer.render(scene, camera)
+    camera.lookAt(obj.position);
+    animate()
+  })
+})
+function animate() {
+  renderer.setClearColor(0xffffff,0.3);
   renderer.render(scene, camera)
   stats.begin()
   stats.end()
   console.log();
   requestAnimationFrame(animate)
-})()
+}
